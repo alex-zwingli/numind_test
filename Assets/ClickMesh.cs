73dc8852f6d6837286f483a91d7e5c414cb0cecd
htinney <hunter.tinney@gmail.com>
2018-02-23 12:54:57 -0700

    Basic model running on a loop, HoloLens deployable

---
+++ Assets/ClickMesh.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HoloToolkit.Unity.InputModule;
+using System;
+
+public class ClickMesh : MonoBehaviour, IInputClickHandler {
+
+    public Animator anim;
+
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        Debug.Log("Input detectged");
+        if (anim != null)
+        {
+            if (anim.GetBool("Paused"))
+                anim.SetBool("Paused", false);
+            else
+                anim.enabled = !anim.enabled;
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
